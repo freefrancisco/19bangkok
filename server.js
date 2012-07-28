7823271bcecf12a7884d56850f33b320ca5eceb1
Francisco Gutierrez <freefrancisco@gmail.com>
2012-07-28 15:33:42 -0700

    tell node to use frontend/index.html instead of game

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ var express   = require( 'express' )
     })
 
 server.get( '/', function( req, res, next ) {
-	if( !req.session.access_token ) {
-		req.session.state = Math.floor( Math.random() * 1100000000000 ).toString()
-		return Betable.authorize( res, req.session.state )
-	}
-	Betable.get( ['account', 'wallet'], req.session.access_token, function( error, data ) {
-	   return res.render( 'game.html', { 
+    if( !req.session.access_token ) {
+        req.session.state = Math.floor( Math.random() * 1100000000000 ).toString()
+        return Betable.authorize( res, req.session.state )
+    }
+    Betable.get( ['account', 'wallet'], req.session.access_token, function( error, data ) {
+       return res.render( 'frontend/index.html', {
             first_name    : data.account.first_name
           , balance       : data.wallet[config.economy].balance
           , access_token  : req.session.access_token
@@ -23,26 +23,26 @@ server.get( '/', function( req, res, next ) {
               , game_id       : config.game_id
             })
         })
-    })	
+    })
 })
 
 server.get( '/callback', function( req, res, next ) {
-	var code  = req.query.code
-	  , state = req.query.state
-	  
+    var code  = req.query.code
+      , state = req.query.state
+
     if( !state || state != req.session.state ) {
         delete req.session.state
         return res.send( 400 )
     }
     delete req.session.state
     if( req.query.error ) return res.send( 'we got an error', req.query.error )
-    
+
     Betable.token( code, function( error, access_token ) {
         if( error ) return res.send( { error: error }, 400 )
 
         req.session.access_token = access_token
         return res.redirect('/')
     })
-})	
+})
 
 server.listen( config.port )
