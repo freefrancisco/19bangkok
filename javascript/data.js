5ebd22bd5fddddd363c1e5b7b8c6c27ca0f2644a
Vivek Chopra <vivekchopra@yahoo.com>
2012-07-28 17:51:07 -0700

    Cleanup of game data, updates to README

--- javascript/data.js
+++ javascript/data.js
@@ -5,21 +5,21 @@
  // Storyline text for situations
  var storyline = [
    {
-     "situation": "You met a nice woman in a bar. She seems like a keeper.",
+     "situation": "You met a nice woman in a bar- she seems like a keeper.",
      "wager": "Wager on how the rest of the evening will go.",
-     "win":"She's great! Time to bring her home to meet mom",
+     "win":"She's a keeper! Time to bring her home to meet mom",
      "lose":"Oh no, she's a ladyboy. Run!"
    },
    {
-     "situation": "You go out to a nice dinner with friends. It looks delicious!",
+     "situation": "You go out for a nice dinner with friends- it looks delicious!",
      "wager": "Wager on how good this meal will really be.",
      "win":"This was the best meal you've ever had!",
-     "lose":"A stomach bug has landed you in the hospital. There goes that vacation.."
+     "lose":"A stomach bug has landed you in the hospital... there goes the vacation!"
    },
    {
      "situation": "You go in search of exotic Thai drugs.",
-     "wager": "Wager on what you got from that guy.",
+     "wager": "Wager on what you got from that guy in the backalley.",
      "win":"You bought some high quality hashish",
-     "lose":"This just bought a bag full of oregano!"
+     "lose":"You bought a bag full of oregano!"
    }
  ];
