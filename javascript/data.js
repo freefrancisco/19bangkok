7e0907878b0c600dc8d36ee856c07d8691003d0d
Vivek Chopra <vivekchopra@yahoo.com>
2012-07-28 17:36:09 -0700

    Initial commit for game data set

---
+++ javascript/data.js
@@ -0,0 +1,25 @@
+/*
+ * Game data
+ */
+
+ // Storyline text for situations
+ var storyline = [
+   {
+     "situation": "You met a nice woman in a bar. She seems like a keeper.",
+     "wager": "Wager on how the rest of the evening will go.",
+     "win":"She's great! Time to bring her home to meet mom",
+     "lose":"Oh no, she's a ladyboy. Run!"
+   },
+   {
+     "situation": "You go out to a nice dinner with friends. It looks delicious!",
+     "wager": "Wager on how good this meal will really be.",
+     "win":"This was the best meal you've ever had!",
+     "lose":"A stomach bug has landed you in the hospital. There goes that vacation.."
+   },
+   {
+     "situation": "You go in search of exotic Thai drugs.",
+     "wager": "Wager on what you got from that guy.",
+     "win":"You bought some high quality hashish",
+     "lose":"This just bought a bag full of oregano!"
+   }
+ ];
