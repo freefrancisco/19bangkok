45e9b77c98c167d0ec177e53102f363271771d05
Gershon Bialer <gershon.bialer@gmail.com>
2012-07-29 12:12:46 -0700

    Final version

--- javascript/data.js
+++ javascript/data.js
@@ -10,20 +10,33 @@
      "wager": "on how the rest of the evening will go.",
      "win":"She's the one! Time to bring her home to meet mom.",
      "lose":"Oh no, she's a ladyboy! Run!",
+	 "background_image":"womanModuleCardOnly.png",
+     "slot_background"  : "womanModuleSlots.png",
+	 "win_image": "womanModuleWin.png",
+	 "lose_image": "womanModuleLose.png"
    },
    {
-     "situation": "You go out for a nice dinner with friends. It looks delicious!",
+     "situation": "",
      "situation_image":"food.png",
-     "wager": "on how good this meal will really be.",
+     "wager": "",
      "win":"This was the best meal you've ever had!",
      "lose":"A stomach bug has landed you in the hospital... there goes the vacation!",
+	 "background_image" : "foodModuleWager.png",
+     "slot_background"  : "foodModuleSlots.png",
+	 "win_image": "foodModuleWin.png",
+	 "lose_image": "foodModuleLose.png"
+
    },
    {
-     "situation": "You go in search of exotic Thai drugs.",
+     "situation": "",
      "situation_image":"dope.png",
-     "wager": "on what you got from that guy in the back alley.",
+     "wager": "",
      "win":"You bought some high quality weed.",
      "lose":"You bought a bag full of oregano!",
+	 "background_image": "drugsModuleBet.png",
+     "slot_background"  : "drugsModuleSlots.png",
+	 "win_image": "drugsModuleWin.png",
+	 "lose_image": "drugsModuleLose.png"
    }
  ];
 
